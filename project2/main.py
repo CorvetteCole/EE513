d40fb1c2079b8115de7653595d0e0f6a878ad846
Cole Gerdemann <cole@coleg.dev>
2023-10-30 19:12:16 -0400

    init init project 2

---
+++ project2/main.py
@@ -0,0 +1,11 @@
+import numpy
+import scipy
+import matplotlib.pyplot as pyplot
+from pathlib import Path
+
+noise_floor = Path('NoiseFloor20180210.wav')
+tone = Path('RestingHeart20180210.wav')
+
+
+
+
