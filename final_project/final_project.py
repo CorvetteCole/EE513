d1e4f9f82039cda9cf354170015f5407148b2d90
Cole Gerdemann <cole@coleg.dev>
2023-12-13 20:08:19 -0500

    add train, predict, k-fold params

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -1,3 +1,5 @@
+import argparse
+import pickle
 from pathlib import Path
 
 import numpy as np
@@ -36,41 +38,52 @@ def extract_fundamental_frequency(audio_signal, sr):
     return f0
 
 
-def main():
-    # Configuration
+def train_and_save_model(x, y):
+    # Train the Gaussian Naive Bayes classifier with all the data
+    clf = GaussianNB()
+    clf.fit(x, y)
+
+    # Save the class prior probabilities, class count, mean feature vectors and covariance matrices
+    model_params = {
+        'priors': clf.class_prior_,
+        'classes_count': clf.class_count_,
+        'theta': clf.theta_,  # Mean features
+        'var': clf.var_,  # Variance (not covariance, as GaussianNB assumes feature independence)
+    }
+
+    # Save the model parameters to a file
+    with open('gaussian_nb_params.pkl', 'wb') as f:
+        pickle.dump(model_params, f)
+
+    print("Model trained with all data and parameters saved.")
+
+
+def load_model_and_classify(x, y):
+    # Load the saved model parameters
+    with open('gaussian_nb_params.pkl', 'rb') as f:
+        model_params = pickle.load(f)
+
+    clf = GaussianNB()
+    clf.class_prior_ = model_params['priors']
+    clf.class_count_ = model_params['classes_count']
+    clf.theta_ = model_params['theta']
+    clf.var_ = model_params['var']
+
+    # Make sure to initialize necessary attributes
+    clf.epsilon_ = 1e-9  # This is needed to avoid divide by zero in predict
+    clf.n_features_in_ = clf.theta_.shape[1]  # Set the number of features
+    clf.classes_ = np.array([0, 1])  # Set the classes if they are binary
+
+    # Perform classification on the new data
+    y_pred = clf.predict(x)
+
+    conf_matrix = confusion_matrix(y, y_pred)
+
+    print_predict_info(y, y_pred, conf_matrix)
+
+
+def k_fold_cross_validation(x, y):
     n_splits = 5
-    sampling_rate = 16000
-    n_features = 3  # Recommended to limit to 12 features
-
-    # Load the audio samples and preprocess them if necessary
-    filepaths_yes = glob.glob(f'{sample_dir}/y*.wav')
-    filepaths_no = glob.glob(f'{sample_dir}/n*.wav')
-
-    # Extract features for each audio file
-    features = []
-
-    for filepath in filepaths_yes + filepaths_no:
-        signal, _ = librosa.load(filepath, sr=sampling_rate)
-        # signal, _ = librosa.effects.trim(signal, top_db=20)
-        signal = librosa.util.normalize(signal)
-
-        # Extract formant frequencies
-        lpc_coefficients = librosa.lpc(signal, order=2 * n_features)
-        formant_features = find_formant_frequencies(lpc_coefficients)[:n_features]
-
-        if len(formant_features) < n_features:
-            formant_features = np.pad(formant_features, (0, n_features - len(formant_features)), 'constant')
-
-        # get fundamental frequency
-        f0 = extract_fundamental_frequency(signal, sampling_rate)
-
-        feature_vector = np.concatenate((formant_features, [f0]))
-
-        features.append(feature_vector)
-
-    x = np.array(features)
-    y = np.array([1] * len(filepaths_yes) + [0] * len(filepaths_no))  # Label "yes" as 1 and "no" as 0
-
     kf = KFold(n_splits=n_splits, shuffle=True)
 
     # Initialize arrays to track performance metrics
@@ -106,8 +119,14 @@ def main():
 
     std_dev_accuracy = np.std(accuracies)
 
+    print_predict_info(true_labels, predicted_labels, conf_matrix)
+
+    print("Standard Deviation of Accuracy over Cross-Validation: {:.4f}".format(std_dev_accuracy))
+
+
+def print_predict_info(y, y_pred, conf_matrix):
     # Now that we have predictions from each fold, compute overall performance metrics
-    precisions, recalls, f1_scores, _ = precision_recall_fscore_support(true_labels, predicted_labels, average=None)
+    precisions, recalls, f1_scores, _ = precision_recall_fscore_support(y, y_pred, average=None)
 
     precision_yes = precisions[1]
     recall_yes = recalls[1]
@@ -117,14 +136,13 @@ def main():
     print("Confusion Matrix:")
     print(conf_matrix)
     print("\nClassification Report:")
-    print(classification_report(true_labels, predicted_labels, target_names=['no', 'yes']))
+    print(classification_report(y, y_pred, target_names=['no', 'yes']))
 
     print("Performance:")
     print("Recall for 'Yes': {:.2f}%".format(100 * recall_yes))
     print("Precision for 'Yes': {:.2f}%".format(100 * precision_yes))
     print("Recall for 'No': {:.2f}%".format(100 * recall_no))
     print("Precision for 'No': {:.2f}%".format(100 * precision_no))
-    print("Standard Deviation of Accuracy over Cross-Validation: {:.4f}".format(std_dev_accuracy))
 
     conf_matrix_formatted = np.array([
         [conf_matrix[0, 0], conf_matrix[0, 1], 100 * precision_no],
@@ -136,5 +154,51 @@ def main():
     print(conf_matrix_formatted)
 
 
+def main(mode):
+    # Configuration
+    sampling_rate = 16000
+    n_features = 3  # Recommended to limit to 12 features
+
+    # Load the audio samples and preprocess them if necessary
+    filepaths_yes = glob.glob(f'{sample_dir}/y*.wav')
+    filepaths_no = glob.glob(f'{sample_dir}/n*.wav')
+
+    # Extract features for each audio file
+    features = []
+
+    for filepath in filepaths_yes + filepaths_no:
+        signal, _ = librosa.load(filepath, sr=sampling_rate)
+        # signal, _ = librosa.effects.trim(signal, top_db=20)
+        signal = librosa.util.normalize(signal)
+
+        # Extract formant frequencies
+        lpc_coefficients = librosa.lpc(signal, order=2 * n_features)
+        formant_features = find_formant_frequencies(lpc_coefficients)[:n_features]
+
+        if len(formant_features) < n_features:
+            formant_features = np.pad(formant_features, (0, n_features - len(formant_features)), 'constant')
+
+        # get fundamental frequency
+        f0 = extract_fundamental_frequency(signal, sampling_rate)
+
+        feature_vector = np.concatenate((formant_features, [f0]))
+
+        features.append(feature_vector)
+
+    x = np.array(features)
+    y = np.array([1] * len(filepaths_yes) + [0] * len(filepaths_no))  # Label "yes" as 1 and "no" as 0
+
+    if mode == 'train':
+        train_and_save_model(x, y)
+    elif mode == 'predict':
+        load_model_and_classify(x, y)
+    elif mode == 'k-fold':
+        k_fold_cross_validation(x, y)
+
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Train a classifier or make predictions.')
+    parser.add_argument('mode', choices=['train', 'predict', 'k-fold'], help='Mode to run the script in.')
+    args = parser.parse_args()
+
+    main(args.mode)
